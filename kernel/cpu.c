84070a20a16265307b775263ee8e13db1014fd82
Greg Kroah-Hartman <gregkh@google.com>
2019-05-14 20:14:26 +0200

    Merge 4.14.119 into android-4.14
    Changes in 4.14.119
    	x86/speculation: Simplify the CPU bug detection logic
    	Documentation/l1tf: Fix small spelling typo
    	x86/cpu: Sanitize FAM6_ATOM naming
    	kvm: x86: Report STIBP on GET_SUPPORTED_CPUID
    	locking/atomics, asm-generic: Move some macros from <linux/bitops.h> to a new <linux/bits.h> file
    	x86/msr-index: Cleanup bit defines
    	x86/speculation: Consolidate CPU whitelists
    	x86/speculation/mds: Add basic bug infrastructure for MDS
    	x86/speculation/mds: Add BUG_MSBDS_ONLY
    	x86/kvm: Expose X86_FEATURE_MD_CLEAR to guests
    	x86/speculation/mds: Add mds_clear_cpu_buffers()
    	x86/speculation/mds: Clear CPU buffers on exit to user
    	x86/kvm/vmx: Add MDS protection when L1D Flush is not active
    	x86/speculation/mds: Conditionally clear CPU buffers on idle entry
    	x86/speculation/mds: Add mitigation control for MDS
    	x86/speculation/mds: Add sysfs reporting for MDS
    	x86/speculation/mds: Add mitigation mode VMWERV
    	Documentation: Move L1TF to separate directory
    	Documentation: Add MDS vulnerability documentation
    	x86/speculation/mds: Add mds=full,nosmt cmdline option
    	x86/speculation: Move arch_smt_update() call to after mitigation decisions
    	x86/speculation/mds: Add SMT warning message
    	x86/speculation/mds: Fix comment
    	x86/speculation/mds: Print SMT vulnerable on MSBDS with mitigations off
    	cpu/speculation: Add 'mitigations=' cmdline option
    	x86/speculation: Support 'mitigations=' cmdline option
    	powerpc/speculation: Support 'mitigations=' cmdline option
    	s390/speculation: Support 'mitigations=' cmdline option
    	x86/speculation/mds: Add 'mitigations=' support for MDS
    	x86/mds: Add MDSUM variant to the MDS documentation
    	Documentation: Correct the possible MDS sysfs values
    	x86/speculation/mds: Fix documentation typo
    	Linux 4.14.119
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -2512,6 +2512,7 @@ void __init boot_cpu_hotplug_init(void)
 	this_cpu_write(cpuhp_state.state, CPUHP_ONLINE);
 }
 
+<<<<<<< HEAD
 /*
  * These are used for a global "mitigations=" cmdline option for toggling
  * optional CPU mitigations.
@@ -2524,6 +2525,9 @@ enum cpu_mitigations {
 
 static enum cpu_mitigations cpu_mitigations __ro_after_init =
 	CPU_MITIGATIONS_AUTO;
+=======
+enum cpu_mitigations cpu_mitigations __ro_after_init = CPU_MITIGATIONS_AUTO;
+>>>>>>> c8c87353e (Merge 4.14.119 into android-4.14)
 
 static int __init mitigations_parse_cmdline(char *arg)
 {
@@ -2533,13 +2537,17 @@ static int __init mitigations_parse_cmdline(char *arg)
 		cpu_mitigations = CPU_MITIGATIONS_AUTO;
 	else if (!strcmp(arg, "auto,nosmt"))
 		cpu_mitigations = CPU_MITIGATIONS_AUTO_NOSMT;
+<<<<<<< HEAD
 	else
 		pr_crit("Unsupported mitigations=%s, system may still be vulnerable\n",
 			arg);
+=======
+>>>>>>> c8c87353e (Merge 4.14.119 into android-4.14)
 
 	return 0;
 }
 early_param("mitigations", mitigations_parse_cmdline);
+<<<<<<< HEAD
 
 /* mitigations=off */
 bool cpu_mitigations_off(void)
@@ -2554,3 +2562,5 @@ bool cpu_mitigations_auto_nosmt(void)
 	return cpu_mitigations == CPU_MITIGATIONS_AUTO_NOSMT;
 }
 EXPORT_SYMBOL_GPL(cpu_mitigations_auto_nosmt);
+=======
+>>>>>>> c8c87353e (Merge 4.14.119 into android-4.14)
